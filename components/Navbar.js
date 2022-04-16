753196974947a2aef4412f0c7e8b7deddf3473a8
mkseo <alsrb1230@naver.com>
2022-04-16 15:54:19 +0900

    fix styles

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,6 +5,7 @@ const NavBar = () => {
   const router = useRouter();
   return (
     <nav>
+      <img src="/vercel.svg" />
       <Link href="/">
         <a className={router.pathname === "/" ? "active" : ""}>HOME</a>
       </Link>
@@ -12,12 +13,30 @@ const NavBar = () => {
         <a className={router.pathname === "/about" ? "active" : ""}>ABOUT</a>
       </Link>
       <style jsx>{`
-        a {
-          text-decoration: none;
-          color: black;
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
         }
         .active {
-          color: red;
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
